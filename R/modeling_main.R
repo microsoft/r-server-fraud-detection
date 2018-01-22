011c4c181cf04d071fab3757c656e75349ae9c14
v-robwhi <v-robwhi@microsoft.com>
2018-01-22 10:33:05 -0800

    Dev (#8)
    * Initial Setup
    
    * removed old files
    
    * update CSV FileNames
    
    * added run once proc
    
    * updated db name
    
    * Fixed CreateSQL
    
    * fixed stuff
    
    * fixed more stuff
    
    * fixed SQL objects
    
    * Taking out training
    
    * fixed parameters
    
    * oops
    
    * fixed DB Reference
    
    * update web settings
    
    * update path in Jupyter notebook
    
    * update name/pw
    
    * updated R code Paths
    
    * update db name
    
    * fixed Juypter Notebook
    
    * fixed dumb JSON error
    
    * changedDB for R COde
    
    * Last Checkin from Dev

--- R/modeling_main.R
+++ R/modeling_main.R
@@ -19,9 +19,10 @@ library(RevoScaleR)
 ##########################################################################################################################################
 
 # Data sets full path. The paths below work if the working directory is set to the R scripts location. 
-Untagged_Transactions <- "../Data/untaggedTransactions.csv"
-Account_Info <- "../Data/accountInfo.csv"
-Fraud_Transactions <- "../Data/fraudTransactions.csv"
+Untagged_Transactions <- "../Data/Untagged_Transactions.csv"
+Account_Info <- "../Data/Account_Info.csv"
+#Fraud <- "../Data/Fraud.csv"
+Fraud_Transactions <- "../Data/Fraud_Transactions.csv"
 
 
 # Creating the connection string. Specify:
--- Resources/ActionScripts/SetupVM.ps1
+++ Resources/ActionScripts/SetupVM.ps1
@@ -1,107 +1,260 @@
-<#
-.SYNOPSIS
-Powershell script for setting up the solution template. 
 
-.DESCRIPTION
-This script checks out the solution from github and deploys it to SQL Server on the local Data Science VM (DSVM).
 
-.WARNING: This script is only meant to be run from the solution template deployment process.
-
-.PARAMETER serverName
-Name of the server with SQL Server with R Services (this is the DSVM server)
-
-.PARAMETER baseurl
-url from which to download data files (if any)
-
-.PARAMETER username
-login username for the server
-
-.PARAMETER password
-login password for the server
-
-.PARAMETER sqlUsername
-User to create in SQL Server
-
-.PARAMETER sqlPassword
-Password for the SQL User
-
-#>
 [CmdletBinding()]
 param(
-[parameter(Mandatory=$true, Position=1, ParameterSetName = "LCR")]
+[parameter(Mandatory=$True, Position=1)]
 [ValidateNotNullOrEmpty()] 
 [string]$serverName,
 
-[parameter(Mandatory=$true, Position=2, ParameterSetName = "LCR")]
-[ValidateNotNullOrEmpty()] 
-[string]$baseurl,
-
-[parameter(Mandatory=$true, Position=3, ParameterSetName = "LCR")]
+[parameter(Mandatory=$True, Position=2)]
 [ValidateNotNullOrEmpty()] 
 [string]$username,
 
-[parameter(Mandatory=$true, Position=4, ParameterSetName = "LCR")]
+[parameter(Mandatory=$True, Position=3)]
 [ValidateNotNullOrEmpty()] 
 [string]$password,
 
-[parameter(Mandatory=$true, Position=5, ParameterSetName = "LCR")]
+[parameter(Mandatory=$false, Position=4)]
 [ValidateNotNullOrEmpty()] 
-[string]$sqlUsername,
-
-[parameter(Mandatory=$true, Position=6, ParameterSetName = "LCR")]
-[ValidateNotNullOrEmpty()] 
-[string]$sqlPassword
+[string]$Prompt
 )
+$startTime = Get-Date
 
-$startTime= Get-Date
-Write-Host "Start time for setup is:" $startTime
-$originalLocation = Get-Location
-# This is the directory for the data/code download
+
+
+#$Prompt= if ($Prompt -match '^y(es)?$') {'Y'} else {'N'}
+$Prompt = 'N'
+
+
+##Change Values here for Different Solutions 
+$SolutionName = "Fraud"
+$SolutionFullName = "r-server-fraud-detection" 
+$JupyterNotebook = "Fraud_Detection_Notebook.ipynb"
+$Shortcut = "frauddetection_Help.url"
+
+
+### DON'T FORGET TO CHANGE TO MASTER LATER...
+$Branch = "master" 
+$InstallR = 'Yes'  ## If Solution has a R Version this should be 'Yes' Else 'No'
+$InstallPy = 'No' ## If Solution has a Py Version this should be 'Yes' Else 'No'
+$SampleWeb = 'Yes' ## If Solution has a Sample Website  this should be 'Yes' Else 'No' 
+$EnableFileStream = 'No' ## If Solution Requires FileStream DB this should be 'Yes' Else 'No' 
+$Prompt = 'N'
+
+
+$setupLog = "c:\tmp\setup_log.txt"
+Start-Transcript -Path $setupLog -Append
+$startTime = Get-Date
+Write-Host -ForegroundColor 'Green'  "  Start time:" $startTime 
+
+
+###These probably don't need to change , but make sure files are placed in the correct directory structure 
 $solutionTemplateName = "Solutions"
 $solutionTemplatePath = "C:\" + $solutionTemplateName
-$checkoutDir = "Fraud"
-
-New-Item -Path "C:\" -Name $solutionTemplateName -ItemType directory -force
-
-$setupLog = $solutionTemplatePath + "\setup_log.txt"
-Start-Transcript -Path $setupLog -Append
-
-cd $solutionTemplatePath
-### DON'T FORGET TO CHANGE TO MASTER LATER...
-git clone  --single-branch https://github.com/Microsoft/r-server-fraud-detection $checkoutDir
+$checkoutDir = $SolutionName
+$SolutionPath = $solutionTemplatePath + '\' + $checkoutDir
+$desktop = "C:\Users\Public\Desktop\"
+$scriptPath = $SolutionPath + "\Resources\ActionScripts\"
+$SolutionData = $SolutionPath + "\Data\"
 
 
-$solutionBase = $solutionTemplatePath + "\" + $checkoutDir 
-$solutionResourcePath = $solutionBase + "\Resources\ActionScripts"
-$helpShortCutFilePath = $solutionResourcePath + "\frauddetection_Help.url"
 
-cd $solutionResourcePath
+####$Query = "SELECT SERVERPROPERTY('ServerName')"
+##$si = invoke-sqlcmd -Query $Query
+##$si = $si.Item(0)
 
-$passwords = $password | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$serverName\$username", $passwords)
-$configure = "configureSolution.ps1"
-$shortcuts ="createShortcuts.ps1"
 
-Enable-PSRemoting -Force
-Invoke-Command  -Credential $credential -ComputerName $serverName -FilePath $configure -ArgumentList $solutionBase, $sqlUsername, $sqlPassword, $checkoutDir
-Invoke-Command  -Credential $credential -ComputerName $serverName -FilePath $shortcuts -ArgumentList $helpShortCutFilePath, $solutionBase
-Disable-PSRemoting -Force
+###$serverName = if($serverName -eq $null) {$si}
 
-Write-Host -ForeGroundColor magenta "Installing latest Power BI..."
+##WRITE-HOST " ServerName set to $ServerName"
+
+
+
+##########################################################################
+#Clone Data from GIT
+##########################################################################
+
+
+$clone = "git clone --branch $Branch --single-branch https://github.com/Microsoft/$SolutionFullName $solutionPath"
+
+if (Test-Path $SolutionPath) { Write-Host " Solution has already been cloned"}
+ELSE {Invoke-Expression $clone}
+
+If ($InstalR -eq 'Yes')
+{
+Write-Host -ForeGroundColor magenta "Installing R Packages"
+Set-Location "C:\Solutions\$SolutionName\Resources\ActionScripts\"
+# install R Packages
+Rscript install.R 
+}
+
+
+#################################################################
+##DSVM Does not have SQLServer Powershell Module Install or Update 
+#################################################################
+
+
+
+Write-Host " Installing SQLServer Power Shell Module or Updating to latest "
+
+if (Get-Module -ListAvailable -Name SQLServer) {Update-Module -Name "SQLServer"}
+ else 
+    {
+    Install-Module -Name SQLServer -Scope AllUsers -AllowClobber -Force
+    Import-Module -Name SQLServer
+    }
+
+
+## if FileStreamDB is Required Alter Firewall ports for 139 and 445
+if ($EnableFileStream -eq 'Yes')
+    {
+    netsh advfirewall firewall add rule name="Open Port 139" dir=in action=allow protocol=TCP localport=139
+    netsh advfirewall firewall add rule name="Open Port 445" dir=in action=allow protocol=TCP localport=445
+    Write-Host -ForeGroundColor cyan " Firewall as been opened for filestream access..."
+    }
+If ($EnableFileStream -eq 'Yes')
+    {
+    Set-Location "C:\Program Files\Microsoft\ML Server\PYTHON_SERVER\python.exe" 
+    .\setup.py install
+    Write-Host -ForeGroundColor cyan " Py Instal has been updated to latest version..."
+    }
+
+
+############################################################################################
+#Configure SQL to Run our Solutions 
+############################################################################################
+
+#Write-Host -ForegroundColor 'Cyan' " Switching SQL Server to Mixed Mode"
+
+
+### Change Authentication From Windows Auth to Mixed Mode 
+Invoke-Sqlcmd -Query "EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2;" -ServerInstance "LocalHost" 
+
+Write-Host -ForeGroundColor 'cyan' " Configuring SQL to allow running of External Scripts "
+### Allow Running of External Scripts , this is to allow R Services to Connect to SQL
+Invoke-Sqlcmd -Query "EXEC sp_configure  'external scripts enabled', 1"
+
+### Force Change in SQL Policy on External Scripts 
+Invoke-Sqlcmd -Query "RECONFIGURE WITH OVERRIDE" 
+Write-Host -ForeGroundColor 'cyan' " SQL Server Configured to allow running of External Scripts "
+
+### Enable FileStreamDB if Required by Solution 
+if ($EnableFileStream -eq 'Yes') 
+    {
+# Enable FILESTREAM
+        $instance = "MSSQLSERVER"
+        $wmi = Get-WmiObject -Namespace "ROOT\Microsoft\SqlServer\ComputerManagement14" -Class FilestreamSettings | where-object {$_.InstanceName -eq $instance}
+        $wmi.EnableFilestream(3, $instance)
+        Stop-Service "MSSQ*" -Force
+        Start-Service "MSSQ*"
+ 
+        Set-ExecutionPolicy Unrestricted
+        #Import-Module "sqlps" -DisableNameChecking
+        Invoke-Sqlcmd "EXEC sp_configure filestream_access_level, 2"
+        Invoke-Sqlcmd "RECONFIGURE WITH OVERRIDE"
+        Stop-Service "MSSQ*"
+        Start-Service "MSSQ*"
+    }
+ELSE
+    { 
+    Write-Host -ForeGroundColor 'cyan' " Restarting SQL Services "
+    ### Changes Above Require Services to be cycled to take effect 
+    ### Stop the SQL Service and Launchpad wild cards are used to account for named instances  
+    Restart-Service -Name "MSSQ*" -Force
+}
+### Start the SQL Service 
+#Start-Service -Name "MSSQ*"
+#Write-Host -ForegroundColor 'Cyan' " SQL Services Restarted"
+
+
+$Query = "CREATE LOGIN $username WITH PASSWORD=N'$password', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF"
+Invoke-Sqlcmd -Query $Query -ErrorAction SilentlyContinue
+
+$Query = "ALTER SERVER ROLE [sysadmin] ADD MEMBER $username"
+Invoke-Sqlcmd -Query $Query -ErrorAction SilentlyContinue
+
+
+####Run Configure SQL to Create Databases and Populate with needed Data
+$ConfigureSql = "C:\Solutions\$SolutionName\Resources\ActionScripts\ConfigureSQL.ps1  $ServerName $SolutionName $InstallPy $InstallR $Prompt"
+Invoke-Expression $ConfigureSQL 
+
+Write-Host -ForegroundColor 'Cyan' " Done with configuration changes to SQL Server"
+
+
+
+
+Write-Host -ForeGroundColor cyan " Installing latest Power BI..."
 # Download PowerBI Desktop installer
 Start-BitsTransfer -Source "https://go.microsoft.com/fwlink/?LinkId=521662&clcid=0x409" -Destination powerbi-desktop.msi
 
 # Silently install PowerBI Desktop
 msiexec.exe /i powerbi-desktop.msi /qn /norestart  ACCEPT_EULA=1
 
-if (!$?)
-{
-    Write-Host -ForeGroundColor Red "Error installing Power BI Desktop. Please install latest Power BI manually."
+if (!$?) {
+    Write-Host -ForeGroundColor Red " Error installing Power BI Desktop. Please install latest Power BI manually."
 }
-cd $originalLocation.Path
-$endTime= Get-Date
-$totalTime = $endTime - $startTime
-Write-Host "Finished running setup at " $endTime
-Write-Host "Total time for setup:" $totalTime
+
+
+##Create Shortcuts and Autostart Help File 
+Copy-Item "$ScriptPath\$Shortcut" C:\Users\Public\Desktop\
+Copy-Item "$ScriptPath\$Shortcut" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
+Write-Host -ForeGroundColor cyan " Help Files Copied to Desktop"
+
+
+$WsShell = New-Object -ComObject WScript.Shell
+$shortcut = $WsShell.CreateShortcut($desktop + $checkoutDir + ".lnk")
+$shortcut.TargetPath = $solutionPath
+$shortcut.Save()
+
+
+
+## copy Jupyter Notebook files
+Move-Item $SolutionPath\R\$JupyterNotebook  c:\tmp\
+sed -i "s/XXYOURSQLPW/$password/g" c:\tmp\$JupyterNotebook
+sed -i "s/XXYOURSQLUSER/$username/g" c:\tmp\$JupyterNotebook
+Move-Item  c:\tmp\$JupyterNotebook $SolutionPath\R\
+
+
+
+
+#cp $SolutionData*.csv  c:\dsvm\notebooks
+ # substitute real username and password in notebook file
+#XXXXXXXXXXChange to NEw NotebookNameXXXXXXXXXXXXXXXXXX# 
+
+if ($InstallPy -eq "Yes")
+{
+    Move-Item $SolutionPath\Python\$JupyterNotebook  c:\tmp\
+    sed -i "s/XXYOURSQLPW/$password/g" c:\tmp\$JupyterNotebook
+    sed -i "s/XXYOURSQLUSER/$username/g" c:\tmp\$JupyterNotebook
+    Move-Item  c:\tmp\$JupyterNotebook $SolutionPath\Python\
+}
+
+# install modules for sample website
+if($SampleWeb  -eq "Yes")
+{
+cd $SolutionPath\Website\
+npm install
+Move-Item $SolutionPath\Website\server.js  c:\tmp\
+sed -i "s/XXYOURSQLPW/$password/g" c:\tmp\server.js
+sed -i "s/XXYOURSQLUSER/$username/g" c:\tmp\server.js
+Move-Item  c:\tmp\server.js $SolutionPath\Website
+}
+
+$endTime = Get-Date
+
+Write-Host -foregroundcolor 'green'(" $SolutionFullName Workflow Finished Successfully!")
+$Duration = New-TimeSpan -Start $StartTime -End $EndTime 
+Write-Host -ForegroundColor 'green'(" Total Deployment Time = $Duration") 
+
 Stop-Transcript
 
+
+##Launch HelpURL 
+Start-Process "https://microsoft.github.io/$SolutionFullName/Typical.html"
+
+
+
+
+## Close Powershell 
+Exit-PSHostProcess
+EXIT 

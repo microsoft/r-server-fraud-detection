661e1e6a2d52697d364739d1b1b8cd0d59c4467e
Bob White <v-robwhi@microsoft.com>
2018-02-27 10:10:37 -0800

    REarranged Sub Scripts

--- Resources/ActionScripts/SetupVM.ps1
+++ Resources/ActionScripts/SetupVM.ps1
@@ -18,7 +18,24 @@ param(
 [ValidateNotNullOrEmpty()] 
 [string]$Prompt
 )
-$startTime = Get-Date
+
+
+#################################################################
+##DSVM Does not have SQLServer Powershell Module Install or Update 
+#################################################################
+
+
+Write-Host "Installing SQLServer Power Shell Module or Updating to latest "
+
+
+if (Get-Module -ListAvailable -Name SQLServer) 
+    {Update-Module -Name "SQLServer" -MaximumVersion 21.0.17199}
+Else 
+    {Install-Module -Name SqlServer -RequiredVersion 21.0.17199 -Scope AllUsers -AllowClobber -Force}
+
+#Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
+    Import-Module -Name SqlServer -MaximumVersion 21.0.17199 -Force
+
 
 
 
@@ -42,10 +59,7 @@ $EnableFileStream = 'No' ## If Solution Requires FileStream DB this should be 'Y
 $Prompt = 'N'
 
 
-$setupLog = "c:\tmp\setup_log.txt"
-Start-Transcript -Path $setupLog -Append
-$startTime = Get-Date
-Write-Host -ForegroundColor 'Green'  "  Start time:" $startTime 
+
 
 
 ###These probably don't need to change , but make sure files are placed in the correct directory structure 
@@ -89,20 +103,7 @@ Rscript install.R
 }
 
 
-#################################################################
-##DSVM Does not have SQLServer Powershell Module Install or Update 
-#################################################################
 
-
-
-Write-Host " Installing SQLServer Power Shell Module or Updating to latest "
-
-if (Get-Module -ListAvailable -Name SQLServer) {Update-Module -Name "SQLServer"}
- else 
-    {
-    Install-Module -Name SQLServer -Scope AllUsers -AllowClobber -Force
-    Import-Module -Name SQLServer
-    }
 
 
 ## if FileStreamDB is Required Alter Firewall ports for 139 and 445
@@ -125,6 +126,11 @@ If ($EnableFileStream -eq 'Yes')
 ############################################################################################
 
 #Write-Host -ForegroundColor 'Cyan' " Switching SQL Server to Mixed Mode"
+
+$setupLog = "c:\tmp\setup_log.txt"
+Start-Transcript -Path $setupLog -Append
+$startTime = Get-Date
+Write-Host -ForegroundColor 'Green'  "  Start time:" $startTime 
 
 
 ### Change Authentication From Windows Auth to Mixed Mode 
